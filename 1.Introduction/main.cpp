51701969b26ba3f5c359692ef6c71a7aa0d33b96
Samarth Kamble <kamble.samarth91@gmail.com>
2025-12-14 20:58:19 +0530

    addition added

--- 1.Introduction/main.cpp
+++ 1.Introduction/main.cpp
@@ -1,8 +1,12 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-    cout<<"Hello World!";
+int main()
+{
+    cout << "Hello World!";
+    int a;
+    int b;
+    cout << a + b;
     return 0;
 }
